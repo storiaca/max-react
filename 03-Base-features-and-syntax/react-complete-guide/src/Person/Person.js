544bbc4f0277f051c379c5cdf8b7e621f01e3cb4
storiaca <storiaca@gmail.com>
2019-05-25 19:23:34 +0200

    Manipulating the State

--- 03-Base-features-and-syntax/react-complete-guide/src/Person/Person.js
+++ 03-Base-features-and-syntax/react-complete-guide/src/Person/Person.js
@@ -1,7 +1,14 @@
 import React from "react";
 
-const Person = () => {
-  return <p>I'm a Person!</p>;
+const Person = props => {
+  return (
+    <React.Fragment>
+      <p>
+        I'm a {props.name} and I am {props.age} years old!
+      </p>
+      <p>{props.children}</p>
+    </React.Fragment>
+  );
 };
 
 export default Person;
