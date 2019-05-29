62c7504b093ca3cd334f61e58034764ea0d55962
Aleksandar Ristic <storiaca@gmail.com>
2019-05-29 17:01:25 +0200

    04 start, Rendering content conditionaly

---
+++ 04-Working-with-Lists-and-Conditionals/src/App.js
@@ -0,0 +1,82 @@
+import React, { Component } from "react";
+//import logo from "./logo.svg";
+import "./App.css";
+import Person from "./Person/Person";
+
+class App extends Component {
+  state = {
+    persons: [
+      { name: "Max", age: 28 },
+      { name: "Manu", age: 29 },
+      { name: "Stephanie", age: 26 }
+    ]
+  };
+
+  switchNameHandler = newName => {
+    //console.log("Was clicked");
+    this.setState({
+      persons: [
+        { name: newName, age: 28 },
+        { name: "Manu", age: 29 },
+        { name: "Stephanie", age: 30 }
+      ]
+    });
+  };
+
+  nameChangedHandler = event => {
+    this.setState({
+      persons: [
+        { name: "Max", age: 28 },
+        { name: event.target.value, age: 29 },
+        { name: "Stephanie", age: 30 }
+      ],
+      otherState: "some other state",
+      showPersons: false
+    });
+  };
+  togglePersonsHandler = () => {
+    this.setState({
+      showPersons: true
+    });
+  };
+  render() {
+    const style = {
+      backgroundColor: "white",
+      font: "inherit",
+      border: "1px solid blue",
+      padding: "8px",
+      cursor: "pointer"
+    };
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+        <button style={style} onClick={this.togglePersonsHandler}>
+          Switch Name
+        </button>
+        {this.state.showPersons ? (
+          <div>
+            <Person
+              name={this.state.persons[0].name}
+              age={this.state.persons[0].age}
+            />
+            <Person
+              name={this.state.persons[1].name}
+              age={this.state.persons[1].age}
+              clickChange={this.switchNameHandler.bind(this, "Maxi!")}
+              changed={this.nameChangedHandler}
+            >
+              My hobbies: Racing
+            </Person>
+            <Person
+              name={this.state.persons[2].name}
+              age={this.state.persons[2].age}
+            />
+          </div>
+        ) : null}
+      </div>
+    );
+  }
+}
+
+export default App;
