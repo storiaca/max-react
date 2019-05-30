4a867f0ed843ddc226e0a175407099d6dc62e832
Aleksandar Ristic <storiaca@gmail.com>
2019-05-30 15:23:26 +0200

    Handling dynamic content the js way

--- 04-Working-with-Lists-and-Conditionals/src/App.js
+++ 04-Working-with-Lists-and-Conditionals/src/App.js
@@ -49,6 +49,30 @@ class App extends Component {
       padding: "8px",
       cursor: "pointer"
     };
+
+    let persons = null;
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          <Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age}
+          />
+          <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+            clickChange={this.switchNameHandler.bind(this, "Maxi!")}
+            changed={this.nameChangedHandler}
+          >
+            My hobbies: Racing
+          </Person>
+          <Person
+            name={this.state.persons[2].name}
+            age={this.state.persons[2].age}
+          />
+        </div>
+      );
+    }
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
@@ -56,26 +80,7 @@ class App extends Component {
         <button style={style} onClick={this.togglePersonsHandler}>
           Toggle Persons
         </button>
-        {this.state.showPersons ? (
-          <div>
-            <Person
-              name={this.state.persons[0].name}
-              age={this.state.persons[0].age}
-            />
-            <Person
-              name={this.state.persons[1].name}
-              age={this.state.persons[1].age}
-              clickChange={this.switchNameHandler.bind(this, "Maxi!")}
-              changed={this.nameChangedHandler}
-            >
-              My hobbies: Racing
-            </Person>
-            <Person
-              name={this.state.persons[2].name}
-              age={this.state.persons[2].age}
-            />
-          </div>
-        ) : null}
+        {persons}
       </div>
     );
   }
