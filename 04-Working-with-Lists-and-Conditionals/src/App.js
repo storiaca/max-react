d647e1ae221731b432e6e7785bce806b4bc41f4d
Aleksandar Ristic <storiaca@gmail.com>
2019-06-05 15:09:15 +0200

    Flexible lists end

--- 04-Working-with-Lists-and-Conditionals/src/App.js
+++ 04-Working-with-Lists-and-Conditionals/src/App.js
@@ -13,15 +13,19 @@ class App extends Component {
   };
 
   nameChangedHandler = (event, id) => {
-    this.setState({
-      persons: [
-        { name: "Max", age: 28 },
-        { name: event.target.value, age: 29 },
-        { name: "Stephanie", age: 30 }
-      ],
-      otherState: "some other state",
-      showPersons: false
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
     });
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({ persons: persons });
   };
   deletePersonHandler = personIndex => {
     // const persons = this.state.persons.slice();
