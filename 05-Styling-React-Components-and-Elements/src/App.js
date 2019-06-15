281624767d480e6cf09e2c1e70fead10db046a15
storiaca <storiaca@gmail.com>
2019-06-15 15:03:36 +0200

    Using Radium for media queries

--- 05-Styling-React-Components-and-Elements/src/App.js
+++ 05-Styling-React-Components-and-Elements/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 //import logo from "./logo.svg";
-import Radium from "radium";
+import Radium, { StyleRoot } from "radium";
 import Person from "./Person/Person";
 
 import "./App.css";
@@ -90,14 +90,16 @@ class App extends Component {
       classes.push("bold"); // classes = ['red', 'bold']
     }
     return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <p className={classes.join("  ")}>This is really working!</p>
-        <button style={style} onClick={this.togglePersonsHandler}>
-          Toggle Persons
-        </button>
-        {persons}
-      </div>
+      <StyleRoot>
+        <div className="App">
+          <h1>Hi, I'm a React App</h1>
+          <p className={classes.join("  ")}>This is really working!</p>
+          <button style={style} onClick={this.togglePersonsHandler}>
+            Toggle Persons
+          </button>
+          {persons}
+        </div>
+      </StyleRoot>
     );
   }
 }
--- 05-Styling-React-Components-and-Elements/src/Person/Person.js
+++ 05-Styling-React-Components-and-Elements/src/Person/Person.js
@@ -2,8 +2,13 @@ import React from "react";
 import Radium from "radium";
 import "./Person.css";
 const Person = props => {
+  const style = {
+    "@media(min-width: 500px)": {
+      width: "450px"
+    }
+  };
   return (
-    <div className="Person">
+    <div className="Person" style={style}>
       <p onClick={props.click}>
         I'm a {props.name} and I am {props.age} years old!
       </p>
