35f3489a887531de9919ba2e40136edf972a6e8e
storiaca <storiaca@gmail.com>
2019-06-15 14:45:47 +0200

    Using Radium end

--- 05-Styling-React-Components-and-Elements/src/App.js
+++ 05-Styling-React-Components-and-Elements/src/App.js
@@ -75,6 +75,10 @@ class App extends Component {
         </div>
       );
       style.backgroundColor = "red";
+      style[":hover"] = {
+        backgroundColor: "salmon",
+        color: "black"
+      };
     }
 
     const classes = [];
