fa9171c308e868d8afb0e1edf37212e024f1a378
storiaca <storiaca@gmail.com>
2019-06-16 10:53:32 +0200

    Enabling and using css modules

--- 05-Styling-React-Components-and-Elements/src/App.js
+++ 05-Styling-React-Components-and-Elements/src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 //import logo from "./logo.svg";
 import Person from "./Person/Person";
 
-import "./App.css";
+import classes from "./App.css";
+
 class App extends Component {
   state = {
     persons: [
@@ -72,18 +73,18 @@ class App extends Component {
       style.backgroundColor = "red";
     }
 
-    const classes = [];
+    const assignedClasses = [];
     if (this.state.persons.length <= 2) {
-      classes.push("red"); // classes = ['red']
+      assignedClasses.push(classes.red); // classes = ['red']
     }
 
     if (this.state.persons.length <= 1) {
-      classes.push("bold"); // classes = ['red', 'bold']
+      assignedClasses.push(classes.bold); // classes = ['red', 'bold']
     }
     return (
-      <div className="App">
+      <div className={classes.App}>
         <h1>Hi, I'm a React App</h1>
-        <p className={classes.join("  ")}>This is really working!</p>
+        <p className={assignedClasses.join("  ")}>This is really working!</p>
         <button style={style} onClick={this.togglePersonsHandler}>
           Toggle Persons
         </button>
