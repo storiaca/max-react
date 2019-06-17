783018aa6b2bb754a179bbe87eb3e49dab35b9f8
Aleksandar Ristic <storiaca@gmail.com>
2019-06-17 13:26:58 +0200

    Section 07-Diving Deeper into Components & React Internals start

---
+++ 07-Deeper-components-and-internals/src/App.js
@@ -0,0 +1,90 @@
+import React, { Component } from "react";
+//import logo from "./logo.svg";
+import Person from "./Person/Person";
+
+import classes from "./App.module.css";
+
+class App extends Component {
+  state = {
+    persons: [
+      { id: "kj98d", name: "Max", age: 28 },
+      { id: "v8ska", name: "Manu", age: 29 },
+      { id: "o4dkl", name: "Stephanie", age: 26 }
+    ]
+  };
+
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({ persons: persons });
+  };
+  deletePersonHandler = personIndex => {
+    // const persons = this.state.persons.slice();
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({
+      persons: persons
+    });
+  };
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+
+    this.setState({
+      showPersons: !doesShow
+    });
+  };
+
+  render() {
+    let persons = null;
+    let btnClass = "";
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return (
+              <Person
+                key={person.id}
+                name={person.name}
+                age={person.age}
+                click={() => this.deletePersonHandler(index)}
+                changed={event => this.nameChangedHandler(event, person.id)}
+              />
+            );
+          })}
+        </div>
+      );
+      btnClass = classes.Red;
+    }
+
+    const assignedClasses = [];
+    if (this.state.persons.length <= 2) {
+      assignedClasses.push(classes.red); // classes = ['red']
+    }
+
+    if (this.state.persons.length <= 1) {
+      assignedClasses.push(classes.bold); // classes = ['red', 'bold']
+    }
+    return (
+      <div className={classes.App}>
+        <h1>Hi, I'm a React App</h1>
+        <p className={assignedClasses.join("  ")}>This is really working!</p>
+        <button onClick={this.togglePersonsHandler} className={btnClass}>
+          Toggle Persons
+        </button>
+        {persons}
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ 07-Deeper-components-and-internals/src/Person/Person.js
@@ -0,0 +1,15 @@
+import React from "react";
+import classes from "./Person.module.css";
+const Person = props => {
+  return (
+    <div className={classes.Person}>
+      <p onClick={props.click}>
+        I'm a {props.name} and I am {props.age} years old!
+      </p>
+      <p>{props.children}</p>
+      <input type="text" onChange={props.changed} value={props.name} />
+    </div>
+  );
+};
+
+export default Person;
