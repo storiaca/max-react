d0baced8344d7f394c51f5433396d41504c3bf49
Aleksandar Ristic <storiaca@gmail.com>
2019-06-17 12:58:07 +0200

    Using error boundaries

--- 05-Styling-React-Components-and-Elements/src/App.js
+++ 05-Styling-React-Components-and-Elements/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 //import logo from "./logo.svg";
 import Person from "./Person/Person";
+import ErrorBoundary from "./ErrorBoundary/ErrorBoundary";
 
 import classes from "./App.css";
 
@@ -52,13 +53,14 @@ class App extends Component {
         <div>
           {this.state.persons.map((person, index) => {
             return (
-              <Person
-                key={person.id}
-                name={person.name}
-                age={person.age}
-                click={() => this.deletePersonHandler(index)}
-                changed={event => this.nameChangedHandler(event, person.id)}
-              />
+              <ErrorBoundary key={person.id}>
+                <Person
+                  name={person.name}
+                  age={person.age}
+                  click={() => this.deletePersonHandler(index)}
+                  changed={event => this.nameChangedHandler(event, person.id)}
+                />
+              </ErrorBoundary>
             );
           })}
         </div>
