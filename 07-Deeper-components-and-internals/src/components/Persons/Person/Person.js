f979ccb08cd5a1028e35913fe7cf85b778723d0c
Aleksandar Ristic <storiaca@gmail.com>
2019-06-19 16:35:55 +0200

    Component creation lifecycle in action

--- 07-Deeper-components-and-internals/src/components/Persons/Person/Person.js
+++ 07-Deeper-components-and-internals/src/components/Persons/Person/Person.js
@@ -1,6 +1,7 @@
 import React from "react";
 import classes from "./Person.module.css";
 const Person = props => {
+  console.log("[Person.js] rendering");
   return (
     <div className={classes.Person}>
       <p onClick={props.click}>
--- 07-Deeper-components-and-internals/src/containers/App.js
+++ 07-Deeper-components-and-internals/src/containers/App.js
@@ -5,6 +5,10 @@ import Cockpit from "../components/Cockpit/Cockpit";
 import classes from "./App.module.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log("[App.js] constructor");
+  }
   state = {
     persons: [
       { id: "kj98d", name: "Max", age: 28 },
@@ -13,6 +17,14 @@ class App extends Component {
     ]
   };
 
+  static getDerivedStateFromProps(props, state) {
+    console.log("[App.js] getDerivedStateFromProps", props);
+    return state;
+  }
+
+  componentDidMount() {
+    console.log("[App.js] comopnentDidMount");
+  }
   nameChangedHandler = (event, id) => {
     const personIndex = this.state.persons.findIndex(p => {
       return p.id === id;
@@ -45,6 +57,7 @@ class App extends Component {
   };
 
   render() {
+    console.log("[App.js] render");
     let persons = null;
     const { appTitle } = this.props;
     if (this.state.showPersons) {
