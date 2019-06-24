38520ad891a1f1386f999ea2781b179355979445
Aleksandar Ristic <storiaca@gmail.com>
2019-06-24 13:07:51 +0200

    Setting state correctly

--- 07-Deeper-components-and-internals/src/containers/App.js
+++ 07-Deeper-components-and-internals/src/containers/App.js
@@ -18,7 +18,8 @@ class App extends Component {
       { id: "o4dkl", name: "Stephanie", age: 26 }
     ],
     showPersons: false,
-    showCockpit: true
+    showCockpit: true,
+    changeCounter: 0
   };
 
   static getDerivedStateFromProps(props, state) {
@@ -51,7 +52,12 @@ class App extends Component {
     const persons = [...this.state.persons];
     persons[personIndex] = person;
 
-    this.setState({ persons: persons });
+    this.setState((prevState, props) => {
+      return {
+        persons: persons,
+        changeCounter: prevState.changeCounter + 1
+      };
+    });
   };
   deletePersonHandler = personIndex => {
     // const persons = this.state.persons.slice();
