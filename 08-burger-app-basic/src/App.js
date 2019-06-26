721b35fdbb6164466843585f59036756b23cf8a2
Aleksandar Ristic <storiaca@gmail.com>
2019-06-26 09:59:11 +0200

    Start implementation of burger buildr

--- 08-burger-app-basic/src/App.js
+++ 08-burger-app-basic/src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
 import Layout from "./components/Layout/Layout";
+import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 class App extends Component {
   render() {
     return (
       <div className="">
-        <Layout>hello</Layout>
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
       </div>
     );
   }
