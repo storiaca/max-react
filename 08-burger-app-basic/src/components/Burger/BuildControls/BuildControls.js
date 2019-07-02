f04df79a921eaf4936ffe29f87b1d465c2564510
Aleksandar Ristic <storiaca@gmail.com>
2019-07-02 10:13:27 +0200

    Adding the Build Control component

---
+++ 08-burger-app-basic/src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const buildControls = props => {
+  return (
+    <div>
+      <p>buildControls</p>
+      <p>buildControls</p>
+    </div>
+  );
+};
+
+export default buildControls;
