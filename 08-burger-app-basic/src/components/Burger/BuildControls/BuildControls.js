25dfb1fd734e884dd92635e9fa818328f4582577
Aleksandar Ristic <storiaca@gmail.com>
2019-07-03 16:39:21 +0200

    Adding the order button

--- 08-burger-app-basic/src/components/Burger/BuildControls/BuildControls.js
+++ 08-burger-app-basic/src/components/Burger/BuildControls/BuildControls.js
@@ -24,6 +24,9 @@ const buildControls = props => {
           />
         );
       })}
+      <button className={classes.OrderButton} disabled={!props.purchaseable}>
+        ORDER NOW
+      </button>
     </div>
   );
 };
--- 08-burger-app-basic/src/containers/BurgerBuilder/BurgerBuilder.js
+++ 08-burger-app-basic/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -17,7 +17,19 @@ class BurgerBuilder extends Component {
       cheese: 0,
       meat: 0
     },
-    totalPrice: 4
+    totalPrice: 4,
+    purchaseable: false
+  };
+
+  updatePurchaseSate = ingredients => {
+    const sum = Object.keys(ingredients)
+      .map(igKey => {
+        return ingredients[igKey];
+      })
+      .reduce((sum, el) => {
+        return sum + el;
+      }, 0);
+    this.setState({ purchaseable: sum > 0 });
   };
 
   addIngredientHandler = type => {
@@ -34,6 +46,7 @@ class BurgerBuilder extends Component {
       ingredients: updatedIngredients,
       totalPrice: newPrice
     });
+    this.updatePurchaseSate(updatedIngredients);
   };
 
   removeIngredientHandler = type => {
@@ -53,6 +66,7 @@ class BurgerBuilder extends Component {
       ingredients: updatedIngredients,
       totalPrice: newPrice
     });
+    this.updatePurchaseSate(updatedIngredients);
   };
   render() {
     const disabledInfo = {
@@ -68,6 +82,7 @@ class BurgerBuilder extends Component {
           ingredientAdded={this.addIngredientHandler}
           ingredientRemoved={this.removeIngredientHandler}
           disabled={disabledInfo}
+          purchaseable={this.state.purchaseable}
           price={this.state.totalPrice}
         />
       </Aux>
