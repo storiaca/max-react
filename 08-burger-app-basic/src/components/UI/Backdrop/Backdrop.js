15d0c911fb596655112cc008c15029daee271fe2
Aleksandar Ristic <storiaca@gmail.com>
2019-07-04 16:01:44 +0200

    Implementing the backdrop component

--- 08-burger-app-basic/src/components/UI/Backdrop/Backdrop.js
+++ 08-burger-app-basic/src/components/UI/Backdrop/Backdrop.js
@@ -1,7 +1,8 @@
 import React from "react";
-
-const Backdrop = props => {
-  return <div>Backdrop</div>;
-};
+import classes from "./Backdrop.module.css";
+const Backdrop = props =>
+  props.show ? (
+    <div className={classes.Backdrop} onClick={props.clicked} />
+  ) : null;
 
 export default Backdrop;
