6ac4b0dfc1b9048a213817f0a8770769c87578f7
Aleksandar Ristic <storiaca@gmail.com>
2019-07-08 12:04:18 +0200

    Adding the price to the Order summary

--- 08-burger-app-basic/src/components/UI/Button/Button.js
+++ 08-burger-app-basic/src/components/UI/Button/Button.js
@@ -2,7 +2,7 @@ import React from "react";
 import classes from "./Button.module.css";
 const Button = props => (
   <button
-    className={[classes.Button, classes[props.btnType]].join("")}
+    className={[classes.Button, classes[props.btnType]].join(" ")}
     onClick={props.clicked}
   >
     {props.children}
