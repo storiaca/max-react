68bd98461f3d03b2c466c687133cb01657b717f8
Aleksandar Ristic <storiaca@gmail.com>
2019-07-08 13:21:11 +0200

    Using a logo in our Application

--- 08-burger-app-basic/src/components/Navigation/Toolbar/Toolbar.js
+++ 08-burger-app-basic/src/components/Navigation/Toolbar/Toolbar.js
@@ -1,11 +1,12 @@
 import React from "react";
+import Logo from "../../Logo/Logo";
 import classes from "./Toolbar.module.css";
 
 const Toolbar = props => {
   return (
     <header className={classes.Toolbar}>
       <div>MENU</div>
-      <div>LOGO</div>
+      <Logo />
       <ul>...</ul>
     </header>
   );
