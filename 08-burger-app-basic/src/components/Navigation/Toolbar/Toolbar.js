d0076921b6ea82e1be71f89a4d74b0e08d6e2361
Aleksandar Ristic <storiaca@gmail.com>
2019-07-08 12:29:26 +0200

    Adding a toolbar

---
+++ 08-burger-app-basic/src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import classes from "./Toolbar.module.css";
+
+const Toolbar = props => {
+  return (
+    <header className={classes.Toolbar}>
+      <div>MENU</div>
+      <div>LOGO</div>
+      <ul>...</ul>
+    </header>
+  );
+};
+
+export default Toolbar;
