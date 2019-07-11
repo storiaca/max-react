48684642a1c525dab65c293bccc1efffa31f68ee
Aleksandar Ristic <storiaca@gmail.com>
2019-07-11 14:23:08 +0200

    Adding a ahmburger icon

--- 08-burger-app-basic/src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggler.js
+++ 08-burger-app-basic/src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggler.js
@@ -1,7 +1,13 @@
 import React from "react";
-
+import classes from "./DrawerToggle.module.css";
 const DrawerToggler = props => {
-  return <div onClick={props.clicked}>MENU</div>;
+  return (
+    <div className={classes.DrawerToggle} onClick={props.clicked}>
+      <div />
+      <div />
+      <div />
+    </div>
+  );
 };
 
 export default DrawerToggler;
