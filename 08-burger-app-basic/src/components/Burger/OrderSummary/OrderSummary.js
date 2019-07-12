9eb98aebc6b4f69c82c83d30c2e1d04f60ff99ae
Aleksandar Ristic <storiaca@gmail.com>
2019-07-12 16:39:35 +0200

    Changing the folder structure

--- 08-burger-app-basic/src/components/Burger/OrderSummary/OrderSummary.js
+++ 08-burger-app-basic/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Aux from "../../../hoc/Aux";
+import Aux from "../../../hoc/Aux/Aux";
 import Button from "../../UI/Button/Button";
 class OrderSummary extends Component {
   // This colud be a functional component, doesn't have to be a class
