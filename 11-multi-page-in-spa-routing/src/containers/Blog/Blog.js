fd97306e46aaae487f7ac4eeb214ebf201ebd7c0
Aleksandar Ristic <storiaca@gmail.com>
2019-07-22 14:48:11 +0200

    Passing route parameters

--- 11-multi-page-in-spa-routing/src/containers/Blog/Blog.js
+++ 11-multi-page-in-spa-routing/src/containers/Blog/Blog.js
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 import { Route, NavLink } from "react-router-dom";
 import Posts from "./Posts/Posts";
 import NewPost from "./NewPost/NewPost";
+import FullPost from "./FullPost/FullPost";
 import "./Blog.css";
 
 class Blog extends Component {
@@ -43,6 +44,7 @@ class Blog extends Component {
         <Route path="/" render={() => <h1>Home 2</h1>} /> */}
         <Route path="/" exact component={Posts} />
         <Route path="/new-post" component={NewPost} />
+        <Route path="/:id" exact component={FullPost} />
       </div>
     );
   }
--- 11-multi-page-in-spa-routing/src/containers/Blog/Posts/Posts.js
+++ 11-multi-page-in-spa-routing/src/containers/Blog/Posts/Posts.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import axios from "../../../axios";
 import Post from "../../../components/Post/Post";
+import { Link } from "react-router-dom";
 import "./Post.css";
 class Posts extends Component {
   state = {
@@ -36,12 +37,13 @@ class Posts extends Component {
     if (!this.state.error) {
       posts = this.state.posts.map(post => {
         return (
-          <Post
-            key={post.id}
-            title={post.title}
-            author={post.author}
-            clicked={() => this.postSelectedHandler(post.id)}
-          />
+          <Link to={"/" + post.id} key={post.id}>
+            <Post
+              title={post.title}
+              author={post.author}
+              clicked={() => this.postSelectedHandler(post.id)}
+            />
+          </Link>
         );
       });
     }
