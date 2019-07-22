497bfee2905c33d73a94a8b7ef473ce7a37452c2
Aleksandar Ristic <storiaca@gmail.com>
2019-07-22 12:39:17 +0200

    Setting up and rendering routes

--- 11-multi-page-in-spa-routing/src/containers/Blog/Blog.js
+++ 11-multi-page-in-spa-routing/src/containers/Blog/Blog.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 // import axios from 'axios';
+import { Route } from "react-router-dom";
 import Posts from "./Posts/Posts";
 import "./Blog.css";
 
@@ -19,7 +20,8 @@ class Blog extends Component {
             </ul>
           </nav>
         </header>
-        <Posts />
+        <Route path="/" exact render={() => <h1>Home</h1>} />
+        <Route path="/" render={() => <h1>Home 2</h1>} />
       </div>
     );
   }
