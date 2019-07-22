b6e9f5c30a65085b78a92f827c8c639f9fdd8641
Aleksandar Ristic <storiaca@gmail.com>
2019-07-22 15:10:26 +0200

    Using switch to load  a single route

--- 11-multi-page-in-spa-routing/src/containers/Blog/Blog.js
+++ 11-multi-page-in-spa-routing/src/containers/Blog/Blog.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 // import axios from 'axios';
-import { Route, NavLink } from "react-router-dom";
+import { Route, NavLink, Switch } from "react-router-dom";
 import Posts from "./Posts/Posts";
 import NewPost from "./NewPost/NewPost";
 import FullPost from "./FullPost/FullPost";
@@ -43,8 +43,10 @@ class Blog extends Component {
         {/* <Route path="/" exact render={() => <h1>Home</h1>} />
         <Route path="/" render={() => <h1>Home 2</h1>} /> */}
         <Route path="/" exact component={Posts} />
-        <Route path="/new-post" component={NewPost} />
-        <Route path="/:id" exact component={FullPost} />
+        <Switch>
+          <Route path="/new-post" component={NewPost} />
+          <Route path="/:id" exact component={FullPost} />
+        </Switch>
       </div>
     );
   }
