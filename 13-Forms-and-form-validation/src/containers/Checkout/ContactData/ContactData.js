a816b0e269cae94e6732c486fed25f33ae4ea758
Aleksandar Ristic <storiaca@gmail.com>
2019-08-06 10:26:42 +0200

    Creating a custom dynamic input component

--- 13-Forms-and-form-validation/src/containers/Checkout/ContactData/ContactData.js
+++ 13-Forms-and-form-validation/src/containers/Checkout/ContactData/ContactData.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import axios from "../../../axios-orders";
 import Spinner from "../../../components/UI/Spinner/Spinner";
 import Button from "../../../components/UI/Button/Button";
+import Input from "../../../components/Input/Input";
 import classes from "./ContactData.module.css";
 class ContactData extends Component {
   state = {
@@ -50,26 +51,26 @@ class ContactData extends Component {
   render() {
     let form = (
       <form>
-        <input
-          className={classes.Input}
+        <Input
+          inputtype="input"
           type="text"
           name="name"
           placeholder="Your Name"
         />
-        <input
-          className={classes.Input}
+        <Input
+          inputtype="input"
           type="email"
           name="email"
           placeholder="Your Email"
         />
-        <input
-          className={classes.Input}
+        <Input
+          inputtype="input"
           type="text"
           name="street"
           placeholder="Street"
         />
-        <input
-          className={classes.Input}
+        <Input
+          inputtype="input"
           type="text"
           name="postal"
           placeholder="Postal Code"
