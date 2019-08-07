d6f86a1a9277200fd738160192d80e10a701c44d
Aleksandar Ristic <storiaca@gmail.com>
2019-08-07 15:06:41 +0200

    Working on an Error

--- 13-Forms-and-form-validation/src/containers/Checkout/ContactData/ContactData.js
+++ 13-Forms-and-form-validation/src/containers/Checkout/ContactData/ContactData.js
@@ -83,6 +83,7 @@ class ContactData extends Component {
           ]
         },
         value: "",
+        validation: {},
         valid: true
       }
     },
@@ -123,7 +124,9 @@ class ContactData extends Component {
 
   checkValidity = (value, rules) => {
     let isValid = true;
-
+    if (!rules) {
+      return true;
+    }
     if (rules.required) {
       isValid = value.trim() !== "" && isValid;
     }
