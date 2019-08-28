4f2e290a616c15b207b931b8588cae59e420e64f
Aleksandar Ristic <storiaca@gmail.com>
2019-08-28 16:12:51 +0200

    Passing and retrieving data with action

--- 14-Redux/redux-max/src/containers/Counter/Counter.js
+++ 14-Redux/redux-max/src/containers/Counter/Counter.js
@@ -36,8 +36,8 @@ const mapDispatchToProps = dispatch => {
   return {
     onIncrementCounter: () => dispatch({ type: "INCREMENT" }),
     onDecrementCounter: () => dispatch({ type: "DECREMENT" }),
-    onAddCounter: () => dispatch({ type: "ADD" }),
-    onSubtractCounter: () => dispatch({ type: "SUBTRACT" })
+    onAddCounter: () => dispatch({ type: "ADD", val: 10 }),
+    onSubtractCounter: () => dispatch({ type: "SUBTRACT", val: 15 })
   };
 };
 export default connect(
--- 14-Redux/redux-max/src/store/reducer.js
+++ 14-Redux/redux-max/src/store/reducer.js
@@ -17,13 +17,13 @@ const reducer = (state = initialState, action) => {
   if (action.type === "ADD") {
     return {
       ...state,
-      counter: state.counter + 5
+      counter: state.counter + action.val
     };
   }
   if (action.type === "SUBTRACT") {
     return {
       ...state,
-      counter: state.counter / 5
+      counter: state.counter - action.val
     };
   }
 
