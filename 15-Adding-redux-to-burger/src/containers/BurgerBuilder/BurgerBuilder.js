44607adbc23b30ada0f20ab9f88c822ab21240ce
storiaca <storiaca@gmail.com>
2019-09-08 12:40:31 +0200

    Redux and UI State

--- 15-Adding-redux-to-burger/src/containers/BurgerBuilder/BurgerBuilder.js
+++ 15-Adding-redux-to-burger/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -12,7 +12,6 @@ import * as actionTypes from "../../store/actions";
 
 class BurgerBuilder extends Component {
   state = {
-    purchaseable: false,
     purchasing: false,
     loading: false,
     error: false
@@ -39,7 +38,7 @@ class BurgerBuilder extends Component {
       .reduce((sum, el) => {
         return sum + el;
       }, 0);
-    this.setState({ purchaseable: sum > 0 });
+    return sum > 0;
   };
 
   purchaseHandler = () => {
@@ -94,7 +93,7 @@ class BurgerBuilder extends Component {
             ingredientAdded={this.props.onIngredientAdded}
             ingredientRemoved={this.props.onIngredientRemoved}
             disabled={disabledInfo}
-            purchaseable={this.state.purchaseable}
+            purchaseable={this.updatePurchaseSate(this.props.ings)}
             price={this.props.price}
             ordered={this.purchaseHandler}
           />
