42cf5e5434922213611c8bdacd72e5d627771db0
Aleksandar Ristic <storiaca@gmail.com>
2019-09-09 14:01:22 +0200

    Using the redux devtools

--- 16-Redux-advanced/src/index.js
+++ 16-Redux-advanced/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore, combineReducers, applyMiddleware } from "redux";
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import counterReducer from "./store/reducers/counter";
 import resultReducer from "./store/reducers/result";
 import { Provider } from "react-redux";
@@ -22,7 +22,16 @@ const logger = store => {
     };
   };
 };
-const store = createStore(rootReducer, applyMiddleware(logger));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(logger))
+);
+// const store = createStore(
+//   rootReducer,
+//   applyMiddleware(logger),
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// );
 ReactDOM.render(
   <Provider store={store}>
     <App />
