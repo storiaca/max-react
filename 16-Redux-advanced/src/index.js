24828c35c11b29e83a6d090b156001bc9642884b
Aleksandar Ristic <storiaca@gmail.com>
2019-09-09 11:31:09 +0200

    16-Redux advanced start fix

--- 16-Redux-advanced/src/index.js
+++ 16-Redux-advanced/src/index.js
@@ -1,23 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { createStore, combineReducers } from "redux";
+import counterReducer from "./store/reducers/counter";
+import resultReducer from "./store/reducers/result";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
-
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import reducer from "./store/reducer";
 
-const store = createStore(reducer);
-const app = (
+const rootReducer = combineReducers({
+  ctr: counterReducer,
+  res: resultReducer
+});
+const store = createStore(rootReducer);
+ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
+    <App />
+  </Provider>,
+  document.getElementById("root")
 );
-ReactDOM.render(app, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
