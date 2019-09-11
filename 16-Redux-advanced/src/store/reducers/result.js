3f93e6b253c00602d7386895df054de3dd69f03e
Aleksandar Ristic <storiaca@gmail.com>
2019-09-11 16:58:56 +0200

    Using utility functions

--- 16-Redux-advanced/src/store/reducers/result.js
+++ 16-Redux-advanced/src/store/reducers/result.js
@@ -1,4 +1,5 @@
 import * as actionTypes from "../actions/actionTypes";
+import { updateObject } from "../utility";
 const initialState = {
   results: []
 };
@@ -7,13 +8,12 @@ const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.STORE_RESULT:
       // Change data
-      return {
-        ...state,
+      return updateObject(state, {
         results: state.results.concat({
           id: new Date(),
           value: action.result * 2
         })
-      };
+      });
     case actionTypes.DELETE_RESULT:
       // const id = 2;
       // const newArray = [...state.results];
@@ -21,10 +21,7 @@ const reducer = (state = initialState, action) => {
       const updatedArray = state.results.filter(
         result => result.id !== action.resultElId
       );
-      return {
-        ...state,
-        results: updatedArray
-      };
+      return updateObject(state, { results: updatedArray });
   }
 
   return state;
