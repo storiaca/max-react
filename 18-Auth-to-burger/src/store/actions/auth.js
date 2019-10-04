376c6cc12f12ac63fcaa6c74926d990c76ee2a9b
Aleksandar Ristic <storiaca@gmail.com>
2019-10-04 16:44:38 +0200

    Fix in auth action

--- 18-Auth-to-burger/src/store/actions/auth.js
+++ 18-Auth-to-burger/src/store/actions/auth.js
@@ -32,7 +32,7 @@ export const auth = (email, password, isSignup) => {
       returnSecureToken: true
     };
     let url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`;
-    if (isSignup) {
+    if (!isSignup) {
       url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${API_KEY}`;
     }
     axios
