1bb00ea285aa6ef2bd1a4108fa6aa51de2915fc3
Aleksandar Ristic <storiaca@gmail.com>
2019-10-04 15:46:31 +0200

    Getting a token from the backend

--- 18-Auth-to-burger/src/store/actions/auth.js
+++ 18-Auth-to-burger/src/store/actions/auth.js
@@ -1,4 +1,7 @@
+import axios from "axios";
 import * as actionTypes from "./actionTypes";
+
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 export const authStart = () => {
   return {
@@ -23,5 +26,23 @@ export const authFail = error => {
 export const auth = (email, password) => {
   return dispatch => {
     dispatch(authStart());
+    const authData = {
+      email: email,
+      password: password,
+      returnSecureToken: true
+    };
+    axios
+      .post(
+        `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`,
+        authData
+      )
+      .then(response => {
+        console.log(response);
+        dispatch(authSuccess(response.data));
+      })
+      .catch(err => {
+        console.log(err);
+        dispatch(authFail());
+      });
   };
 };
