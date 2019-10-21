9137e89389426cae457beeeafd68e7d0ff451878
Aleksandar Ristic <storiaca@gmail.com>
2019-10-21 14:43:20 +0200

    Using updateObject in the entire app

--- 19-Improving-burger-project/src/containers/Auth/Auth.js
+++ 19-Improving-burger-project/src/containers/Auth/Auth.js
@@ -5,6 +5,7 @@ import Input from "../../components/UI/Input/Input";
 import Button from "../../components/UI/Button/Button";
 import Spinner from "../../components/UI/Spinner/Spinner";
 import * as actions from "../../store/actions/index";
+import { updateObject } from "../../shared/utility";
 import classes from "./Auth.module.css";
 class Auth extends Component {
   state = {
@@ -79,18 +80,16 @@ class Auth extends Component {
   }
 
   inputChangedHandler = (event, controlName) => {
-    const updatedControls = {
-      ...this.state.controls,
-      [controlName]: {
-        ...this.state.controls[controlName],
+    const updatedControls = updateObject(this.state.controls, {
+      [controlName]: updateObject(this.state.controls[controlName], {
         value: event.target.value,
         valid: this.checkValidity(
           event.target.value,
           this.state.controls[controlName].validation
         ),
         touched: true
-      }
-    };
+      })
+    });
     this.setState({ controls: updatedControls });
   };
 
