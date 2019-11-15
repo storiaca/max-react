f84e828b68705dc38856a0d4ab63778ba242c16b
Aleksandar Ristic <storiaca@gmail.com>
2019-11-15 15:48:01 +0100

    Creating the basic react application

---
+++ 22-working-with-webpack/src/components/PizzaImage/PizzaImage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import classes from "./PizzaImage.css";
+import PizzaImage from "../../assets/pizza.jpg";
+
+const PizzaImage = props => {
+  return (
+    <div className={classes.PizzaImage}>
+      <img src={PizzaImage} alt={PizzaImage} className={classes.PizzaImg} />
+    </div>
+  );
+};
+
+export default PizzaImage;
