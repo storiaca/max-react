0a9a1617add7b56e4f4c59836160ffeb94528836
storiaca <storiaca@gmail.com>
2019-11-30 15:00:14 +0100

    Preparing the demo project

--- 24-animations-react-apps/src/components/Modal/Modal.js
+++ 24-animations-react-apps/src/components/Modal/Modal.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React from "react";
 
-import './Modal.css';
+import "./Modal.css";
 
-const modal = (props) => (
+const modal = props => {
+  const cssClasses = ["Modal", props.show ? "ModalOpen" : "ModalClosed"];
+  return (
     <div className="Modal">
-        <h1>A Modal</h1>
-        <button className="Button" onClick={props.closed}>Dismiss</button>
+      <h1>A Modal</h1>
+      <button className={cssClasses.join(" ")} onClick={props.closed}>
+        Dismiss
+      </button>
     </div>
-);
+  );
+};
 
 export default modal;
