1b0a1b229fde977a667bd696c243330bd7d128cc
Aleksandar Ristic <storiaca@gmail.com>
2019-12-16 10:29:53 +0100

    Moving the Orders side effects into sagas

--- 25-redux-saga/src/store/actions/index.js
+++ 25-redux-saga/src/store/actions/index.js
@@ -6,7 +6,17 @@ export {
   fetchIngredientsFailed
 } from "./burgerBuilder";
 
-export { purchaseBurger, purchaseInit, fetchOrders } from "./order";
+export {
+  purchaseBurger,
+  purchaseInit,
+  fetchOrders,
+  purchaseBurgerStart,
+  purchaseBurgerFail,
+  purchaseBurgerSuccess,
+  fetchOrdersStart,
+  fetchOrdersSuccess,
+  fetchOrdersFail
+} from "./order";
 
 export {
   auth,
--- 25-redux-saga/src/store/sagas/index.js
+++ 25-redux-saga/src/store/sagas/index.js
@@ -10,6 +10,8 @@ import {
 
 import { initIngredientsSaga } from "./burgerBuilder";
 
+import { purchaseBurgerSaga, fetchOrdersSaga } from "./order";
+
 export function* watchAuth() {
   yield takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkAuthTimeoutSaga);
   yield takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logoutSaga);
@@ -19,4 +21,9 @@ export function* watchAuth() {
 
 export function* watchBurgerBuilder() {
   yield takeEvery(actionTypes.INIT_INGREDIENTS, initIngredientsSaga);
+}
+
+export function* watchOrder() {
+  yield takeEvery(actionTypes.PURCHASE_BURGER, purchaseBurgerSaga);
+  yield takeEvery(actionTypes.FETCH_ORDERS, fetchOrdersSaga);
 }
