26e779fec01e4dfb4b6101ceec8266ea4b5a95af
Aleksandar Ristic <storiaca@gmail.com>
2019-12-30 15:44:53 +0100

    Getting started with custom hooks

--- 26-react-hooks/src/components/Ingredients/Ingredients.js
+++ 26-react-hooks/src/components/Ingredients/Ingredients.js
@@ -18,26 +18,9 @@ const ingredientReducer = (currentIngredients, action) => {
   }
 };
 
-const httpReducer = (curHttpState, action) => {
-  switch (action.type) {
-    case "SEND":
-      return { loading: true, error: null };
-    case "RESPONSE":
-      return { ...curHttpState, loading: false };
-    case "ERROR":
-      return { loading: false, error: action.errorMessage };
-    case "CLEAR":
-      return { ...curHttpState, error: null };
-    default:
-      throw new Error("Should not be reached");
-  }
-};
 function Ingredients() {
   const [userIngredients, dispatch] = useReducer(ingredientReducer, []);
-  const [httpState, dispatchHttp] = useReducer(httpReducer, {
-    loading: false,
-    error: null
-  });
+
   //const [userIngredients, setUserIngredients] = useState([]);
   // const [isLoading, setIsLoading] = useState(false);
   // const [error, setError] = useState();
@@ -76,22 +59,6 @@ function Ingredients() {
 
   const removeIngredientHandler = useCallback(ingredientId => {
     dispatchHttp({ type: "SEND" });
-    fetch(
-      `https://react-hooks-update-560ef.firebaseio.com/ingredients/${ingredientId}.json`,
-      {
-        method: "DELETE"
-      }
-    )
-      .then(response => {
-        dispatchHttp({ type: "RESPONSE" });
-        // setUserIngredients(prevIngredients =>
-        //   prevIngredients.filter(item => item.id !== ingredientId)
-        // );
-        dispatch({ type: "DELETE", id: ingredientId });
-      })
-      .catch(error => {
-        dispatchHttp({ type: "ERROR", errorMessage: error.message });
-      });
   }, []);
 
   const clearError = useCallback(() => {
---
+++ 26-react-hooks/src/hooks/http.js
@@ -0,0 +1,44 @@
+import { useReducer } from "react";
+const httpReducer = (curHttpState, action) => {
+  switch (action.type) {
+    case "SEND":
+      return { loading: true, error: null, data: null };
+    case "RESPONSE":
+      return { ...curHttpState, loading: false, data: action.responseData };
+    case "ERROR":
+      return { loading: false, error: action.errorMessage };
+    case "CLEAR":
+      return { ...curHttpState, error: null };
+    default:
+      throw new Error("Should not be reached");
+  }
+};
+const useHttp = () => {
+  const [httpState, dispatchHttp] = useReducer(httpReducer, {
+    loading: false,
+    error: null,
+    data: null
+  });
+
+  const sendRequest = (url, method, body) => {
+    dispatchHttp({ type: "SEND" });
+    fetch(url, {
+      method: method,
+      body: body,
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(response => {
+        return response.json();
+      })
+      .then(responseData => {
+        dispatchHttp({ type: "RESPONSE", responseData: responseData });
+      })
+      .catch(error => {
+        dispatchHttp({ type: "ERROR", errorMessage: error.message });
+      });
+  };
+};
+
+export default useHttp;
