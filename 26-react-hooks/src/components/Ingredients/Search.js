6796b815b63c37d716da4f69508acca29fd2f18a
Aleksandar Ristic <storiaca@gmail.com>
2020-01-08 16:57:39 +0100

    Using the Custom Hook - Search.js komponent start

--- 26-react-hooks/src/components/Ingredients/Search.js
+++ 26-react-hooks/src/components/Ingredients/Search.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect, useRef } from "react";
 
 import Card from "../UI/Card";
+import useHttp from "../../hooks/http";
 import "./Search.css";
 
 const Search = React.memo(props => {
   const { onLoadIngredients } = props;
   const [enteredFilter, setEnteredFilter] = useState("");
   const inputRef = useRef();
+  const { isLoading, data, error, sendRequest, clear } = useHttp();
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -15,22 +17,21 @@ const Search = React.memo(props => {
           enteredFilter.length === 0
             ? ""
             : `?orderBy="title"&equalTo="${enteredFilter}"`;
-        fetch(
+        sendRequest(
           "https://react-hooks-update-560ef.firebaseio.com/ingredients.json" +
-            query
-        )
-          .then(response => response.json())
-          .then(responseData => {
-            const loadedIngredients = [];
-            for (const key in responseData) {
-              loadedIngredients.push({
-                id: key,
-                title: responseData[key].title,
-                amount: responseData[key].amount
-              });
-            }
-            onLoadIngredients(loadedIngredients);
-          });
+            query,
+          "GET"
+        );
+        //   const loadedIngredients = [];
+        //   for (const key in responseData) {
+        //     loadedIngredients.push({
+        //       id: key,
+        //       title: responseData[key].title,
+        //       amount: responseData[key].amount
+        //     });
+        //   }
+        //   onLoadIngredients(loadedIngredients);
+        // });
       }
     }, 500);
     return () => {
