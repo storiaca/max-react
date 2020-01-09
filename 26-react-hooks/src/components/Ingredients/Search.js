ec0588ab651993d3f9d7aad9eae0317b3d8f8d9f
Aleksandar Ristic <storiaca@gmail.com>
2020-01-09 10:03:54 +0100

    Using the Custom Hook-Search.js finish

--- 26-react-hooks/src/components/Ingredients/Search.js
+++ 26-react-hooks/src/components/Ingredients/Search.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 
 import Card from "../UI/Card";
+import ErrorModal from "../UI/ErrorModal";
 import useHttp from "../../hooks/http";
 import "./Search.css";
 
@@ -22,28 +23,34 @@ const Search = React.memo(props => {
             query,
           "GET"
         );
-        //   const loadedIngredients = [];
-        //   for (const key in responseData) {
-        //     loadedIngredients.push({
-        //       id: key,
-        //       title: responseData[key].title,
-        //       amount: responseData[key].amount
-        //     });
-        //   }
-        //   onLoadIngredients(loadedIngredients);
-        // });
       }
     }, 500);
     return () => {
       clearTimeout(timer);
     };
-  }, [enteredFilter, onLoadIngredients, inputRef]);
+  }, [enteredFilter, inputRef, sendRequest]);
+
+  useEffect(() => {
+    if (!isLoading && !error && data) {
+      const loadedIngredients = [];
+      for (const key in data) {
+        loadedIngredients.push({
+          id: key,
+          title: data[key].title,
+          amount: data[key].amount
+        });
+      }
+      onLoadIngredients(loadedIngredients);
+    }
+  }, [data, isLoading, error, onLoadIngredients]);
 
   return (
     <section className="search">
+      {error && <ErrorModal onClose={clear}>{error}</ErrorModal>}
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
+          {isLoading && <span>Loading...</span>}
           <input
             ref={inputRef}
             type="text"
