25577730da8dc91021446b126f72edb384baab49
Mac <storiaca@gmail.com>
2020-01-27 12:55:38 +0100

    Alternate: Using the Context API

--- 28-replacing-redux-with-react-hooks/src/index.js
+++ 28-replacing-redux-with-react-hooks/src/index.js
@@ -1,24 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { combineReducers, createStore } from 'redux';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import './index.css';
-import App from './App';
-import productReducer from './store/reducers/products';
+import { BrowserRouter } from "react-router-dom";
 
-const rootReducer = combineReducers({
-  shop: productReducer
-});
-
-const store = createStore(rootReducer);
+import "./index.css";
+import App from "./App";
+import ProductsProvider from "./context/products-context";
 
 ReactDOM.render(
-  <Provider store={store}>
+  <ProductsProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </Provider>,
-  document.getElementById('root')
+  </ProductsProvider>,
+  document.getElementById("root")
 );
